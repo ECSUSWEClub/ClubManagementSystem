170d3e233f1788602bf775c164e7ebc93b489d2a
JoshNeil <pocketgamer74@gmail.com>
2025-04-15 23:51:00 -0400

    #86 - Created feedback table model
    Added the model "Feedback" to models.py and created the 3 fields described in the ticket

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Feedback(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    suggestion = models.TextField()
+    date_submitted = models.DateField(auto_now_add=True)
