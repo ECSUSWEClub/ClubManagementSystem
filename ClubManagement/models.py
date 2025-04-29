59eab2ac97c7b2d40a4c794660fecee244b6b29c
Dan-Estrin <danielestrin7@gmail.com>
2025-04-29 14:29:33 -0400

    added Skills model w/user_id as FK to User, skill_name as VCHAR, proficiency as INT

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+  #user class doesnt exsist in this, stand in User
+  user_id = models.ForeignKey(User, max_length=10)
+  skill_name = models.CharField(max_length=255)
+  proficiency = models.IntegerField()
