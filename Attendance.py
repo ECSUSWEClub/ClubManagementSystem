36916ea575cdb59c424c11e2193aa7e0c7598f36
HyrumDescends <156871072+HyrumDG@users.noreply.github.com>
2025-04-30 14:42:07 -0400

    Create Attendance.py

---
+++ Attendance.py
@@ -0,0 +1,13 @@
+import django.db
+
+#thing for Enum
+parameters = ['Present','Excused','Absent',]
+
+class Attendance(models.Model):
+    #fields
+    meeting_id = models.IntegerField()
+    user_id = models.IntegerField()
+    status = models.CharField(max_length = 1, choices = parameters)
+
+    def __str__(self):
+        return self.name
