819614e435ac52191871e55041e2e21882de6469
natalie <181400835+natalieweronik@users.noreply.github.com>
2025-04-30 01:23:04 -0400

    Update models.py

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(model.Model):
+    title = models.CharField(max_length = 100)
+    description = models.TextField()
+    due_date = models.DateField()
+    completed = models.BooleanField(default = False)
+    assign_to_user = models.ForeignKey(User, on_delete = models.CASCADE)
+    assigned_role = models.CharField(max_length = 50)
+    created_by = models.ForeignKey(User, on_delete = models.CASCADE)
