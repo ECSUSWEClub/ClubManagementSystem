1cef4e3776fbca4b824a33a274583a2d80ee1af4
Dan-Estrin <danielestrin7@gmail.com>
2025-04-30 16:09:45 -0400

    added documentsections table

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -6,3 +6,8 @@ class Skill(models.Model):
   user_id = models.ForeignKey(User, max_length=10)
   skill_name = models.CharField(max_length=255)
   proficiency = models.IntegerField()
+
+class DocumentationSections(models.Model):
+  documentation_id = models.ForeignKey(Documentation, max_length=10)
+  section_title = models.CharField(max_length=255)
+  section_text = models.TextField()
