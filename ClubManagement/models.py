212e7e9972126ea45611738efa7205d7474e40ef
JoshNeil <pocketgamer74@gmail.com>
2025-04-30 16:53:46 -0400

    #91 - Created SurveyQuestions Model
    Added the model "Survey Questions" to models.py and created the 3 fields described in the ticket. Question types exist as a dictionary as outlined in the documentation and can be modified as needed.

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class SurveyQuestions(models.Model):
+
+    QUESTION_TYPES = {
+        "multiple-choice" : "Multiple Choice",
+        "text" : "Text",
+        "checkbox" : "Checkbox",
+        "dropdown" : "Dropdown",
+        "true-false" : "True or False"
+    }
+
+    survey_id = models.ForeignKey(Surveys, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    question_type = models.CharField(max_length=20, choices=QUESTION_TYPES, default="text")
