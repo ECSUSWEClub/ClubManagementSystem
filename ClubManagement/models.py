e9e67227ca5811c30387bc11513a3b2dd088bd22
Gdude04 <garvinfrancois@gmail.com>
2025-04-30 15:11:35 -0400

    Update models.py

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class SuggestionTable(models.Model):
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey.IntegerField(User, max_length = 10)
     suggestion = models.TextField()
     date_submitted = models.DateField()
