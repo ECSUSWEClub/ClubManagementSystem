3b0447f2d4de00e19402ad95590152425968bca9
garevalo031308 <116689872+garevalo031308@users.noreply.github.com>
2025-05-01 13:21:03 -0400

    Update models.py

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Documentation(models.Model):
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    last_edited_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
