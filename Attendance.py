c3d5b34c623485ef3d9da92d50dc95b76e0f0011
HyrumDescends <156871072+HyrumDG@users.noreply.github.com>
2025-05-01 13:18:05 -0400

    Update Attendance.py

--- Attendance.py
+++ Attendance.py
@@ -5,8 +5,8 @@ parameters = ['Present','Excused','Absent',]
 
 class Attendance(models.Model):
     #fields
-    meeting_id = models.IntegerField(Meeting)
-    user_id = models.IntegerField(Users)
+    meeting_id = models.ForeignKey(Meeting)
+    user_id = models.ForeignKey(Users)
     status = models.CharField(max_length = 1, choices = parameters)
 
     def __str__(self):
