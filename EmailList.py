e657ed89fba4f30213ce4ad882b9283399d47ba5
HyrumDescends <156871072+HyrumDG@users.noreply.github.com>
2025-05-01 13:17:37 -0400

    Update EmailList.py

--- EmailList.py
+++ EmailList.py
@@ -3,8 +3,8 @@ from django.db import models
 #model class
 class EmailList(models.Model):
     #fields
-    user_id=models.IntegerField(Users)
-    club_season_id=models.IntegerField(Club)
+    user_id=models..ForeignKey(Users)
+    club_season_id=models..ForeignKey(Club)
     subscribed=models.BooleanField()
     #Stack overflow had this, I assume it returns readable name of class
     def __str__(self):
