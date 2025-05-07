20b8b8885642751d74349d2a98964474b2176324
Ashton <cheethama@my.easternct.edu>
2025-05-07 15:14:01 -0400

    Update models.py

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Club(models.Model):
+    SEMESTER = {
+        "F": "Fall",
+        "W": "Winter"
+    }
+    semester = models.CharField(max_length=1, choices=SEMESTER)
+    year = models.IntegerField()
+    project_name = models.CharField(max_length=50)
+    project_description = models.TextField()
+
+
 # Create your models here.
