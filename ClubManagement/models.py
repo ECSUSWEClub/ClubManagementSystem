5ccd91f9b3fec91aa7539c1cc0bd0ad615cbd89c
Jordi Cassar <186864784+jordicassar@users.noreply.github.com>
2025-05-07 15:14:59 -0400

    Update models.py

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+class Surveys(models.model):
+    title = models.Charfield(max_length=50),
+    created_by = models.ForeignKey(Users, on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
