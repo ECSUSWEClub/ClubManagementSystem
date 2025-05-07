0b6429295e4b48d42d60bc038517b6d1c74d551d
coldluma <coldluma@gmail.com>
2025-05-07 15:10:11 -0400

    Added Events table to models.py

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.TextField()
+    event_date = models.DateField()
+    event_time = models.TimeField()
+    location = models.CharField(max_length=100)
+    club_session_id = models.ForeignKey(Club)
