e9a009757b991f0c2a2732d3a2c9551b3341f210
Jordi Cassar <186864784+jordicassar@users.noreply.github.com>
2025-05-07 15:22:44 -0400

    Update models.py

--- ClubManagement/models.py
+++ ClubManagement/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Table_users(models.Model): 
+    first_name = models.CharField(max_length= 50),
+    last_name = models.CharField(max_length= 50),
+    member_username = models.CharField(max_length= 50),
+    discord_username = models.CharField(max_length= 50),
+    github_username = models.CharField(max_length= 50),
+    email = models.CharField(max_length=50),
+    role = models.CharField(max_length= 50, choices= [
+     ('MEMBER', 'Member'),  
+     ('PRESIDENT', 'President'),
+     ('VICE PRESIDENT', 'Vice President'),
+     ('PUBLIC RELATIONS OFFICER', 'Public Relations Officer'),
+     ('TREASURER', 'Treasurer'),
+     ('SECRETARY', 'Secretary')
+    ])
+    password = models.CharField(max_length=128)
+    date_joined = models.DateField(auto_now_add=True)
+    active = models.BooleanField(),
+    alumni = models.BooleanField(),
+    
+    club = models.ForeignKey(Club, on_delete = models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
